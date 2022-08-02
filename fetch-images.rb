af1d6e92d4e0a26426106327d022f7630e902766
Spharian <s.fertons@gmail.com>
2022-08-02 12:44:04 +0200

    rjust optimization

--- fetch-images.rb
+++ fetch-images.rb
@@ -5,7 +5,7 @@ dir = "./images"
 Dir.mkdir(dir) unless File.exists?(dir)
 
 for i in 0..86 do
-  link = "https://www.apple.com/105/media/us/macbook-pro-14-and-16/2021/a1c5d17e-d8e4-4fa8-b70a-bc61bd266412/anim/hero-specs//large/large_00#{i.to_s.rjust(2, '0')}.jpg"
+  link = "https://www.apple.com/105/media/us/macbook-pro-14-and-16/2021/a1c5d17e-d8e4-4fa8-b70a-bc61bd266412/anim/hero-specs//large/large_#{i.to_s.rjust(4, '0')}.jpg"
 
   puts link
 
